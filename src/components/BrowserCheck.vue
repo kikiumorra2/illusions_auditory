4601da8e716bf530b2dd297c15e486c3ddc4faa5
kikiumorra2 <kikiumorra2@gmail.com>
2026-09-14 15:20:26 -0400

    Update BrowserCheck.vue

--- src/components/BrowserCheck.vue
+++ src/components/BrowserCheck.vue
@@ -86,7 +86,7 @@ export default {
 
   computed: {
     previewSentence() {
-      return "If you do not see this entire sentence in one line on your screen, please widen your window until it fits. Please do this before you start. Thank you!.";  
+      return "If you do not see this entire text in one line on your screen, please widen your window until it fits.--Please do this before you start.--Thank you!";  
     },
   },
 };
